01410f053bd081f4ca6dddc2d747550942bd0f6e
DilipX07 <dilipas101@gmail.com>
2026-06-20 18:11:08 +0530

    Run SQLite migrations during Render build

--- build.sh
+++ build.sh
@@ -4,3 +4,5 @@ set -o errexit
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py migrate --no-input
+python manage.py seed_scanner_login
